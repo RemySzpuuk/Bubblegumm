fc300d287211bc4d099ef956b0ee69a8bd914895
RemySzpuuk <elijoffee@gmail.com>
2021-01-20 12:17:07 +0000

    Organisation

--- generator.py
+++ generator.py
@@ -1,1 +1,14 @@
-## sitemap generator
+from git.manager import Manager # Used to read .gitignore
+from git.scanner import Scanner # Used to scan for files/directories
+from bs4 import beatifulsoup as BS # Used to generate sitemap html
+
+# Read .gitignore
+# Analyse .gitignore
+
+# Read directories and files
+# Remove any .gitignore files or file types
+
+# Generate a basic html sitemap
+            
+
+
